38d9984beafb84b421dafab7c1a2c2d6e7428789
jatin <getjatin2010@live.com>
2016-07-28 00:15:07 +0530

    renamingActivityFile

--- app/src/main/java/in/pecfest/www/pecfest/mainPage.java
+++ app/src/main/java/in/pecfest/www/pecfest/mainPage.java
@@ -8,6 +8,6 @@ public class mainPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_the_main_screen);
+        setContentView(R.layout.activity_main_page);
     }
 }
