8c53a992fa7b24ac5352195acd8b87f28a1934fe
jatin <getjatin2010@live.com>
2016-07-28 00:08:17 +0530

    base Commit

---
+++ app/src/main/java/in/pecfest/www/pecfest/mainPage.java
@@ -0,0 +1,13 @@
+package in.pecfest.www.pecfest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_the_main_screen);
+    }
+}
