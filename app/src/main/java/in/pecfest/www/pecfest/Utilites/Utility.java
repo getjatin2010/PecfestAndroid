7e65a1482fe8cb550416d42727b9523834f30e7e
jatin <getjatin2010@live.com>
2016-08-13 09:22:55 +0530

    Removing Scroll View and adding functionally of saving password

--- app/src/main/java/in/pecfest/www/pecfest/Utilites/Utility.java
+++ app/src/main/java/in/pecfest/www/pecfest/Utilites/Utility.java
@@ -1,7 +1,12 @@
 package in.pecfest.www.pecfest.Utilites;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBar;
+import android.text.InputType;
+import android.widget.EditText;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -9,6 +14,7 @@ import com.google.gson.GsonBuilder;
 import in.pecfest.www.pecfest.Communication.ExecuteRequest;
 import in.pecfest.www.pecfest.Interfaces.CommunicationInterface;
 import in.pecfest.www.pecfest.Model.Common.Request;
+import in.pecfest.www.pecfest.R;
 
 
 /**
@@ -42,4 +48,44 @@ public class Utility {
     {
         actionBar.setTitle(title);
     }
+
+    public static void saveId(String id,Context context)
+    {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("appPreferences",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor  =sharedPreferences.edit();
+        editor.putString("pecId",id);
+        editor.commit();
+    }
+
+    public static String getsaveId(Context context)
+    {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("appPreferences",Context.MODE_PRIVATE);
+        return sharedPreferences.getString("pecId","");
+    }
+
+
+    private void showDialogForPecfestID(final Context context)
+    {
+        final AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+        final EditText input = new EditText(context);
+        alertDialog.setTitle("Enter Your PecfestId");
+        alertDialog.setIcon(R.mipmap.ic_launcher);
+        input.setInputType(InputType.TYPE_CLASS_TEXT );
+        String pecfestOld = Utility.getsaveId(context);
+        input.setHint("PecfestID");
+        if(pecfestOld!="")
+            input.setText(pecfestOld);
+        alertDialog.setView(input);
+        alertDialog.setButton(DialogInterface.BUTTON_NEUTRAL, "DONE", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                String pecfestId = input.getText().toString();
+                if (pecfestId != null) {
+                    Utility.saveId(pecfestId,context);
+                }
+            }
+        });
+        alertDialog.show();
+
+    }
+
 }
