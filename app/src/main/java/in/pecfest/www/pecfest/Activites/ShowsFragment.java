b815ddd6e6597b50196caa09807921b933336e6b
Abhinandan <abhics27@gmail.com>
2016-09-11 00:13:01 +0530

    NA-0: Added shows

--- app/src/main/java/in/pecfest/www/pecfest/Activites/ShowsFragment.java
+++ app/src/main/java/in/pecfest/www/pecfest/Activites/ShowsFragment.java
@@ -1,27 +1,27 @@
 package in.pecfest.www.pecfest.Activites;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import in.pecfest.www.pecfest.Communication.HttpConnection;
 import in.pecfest.www.pecfest.Communication.ImageLoader;
-import in.pecfest.www.pecfest.Interfaces.CommunicationInterface;
 import in.pecfest.www.pecfest.Model.Common.Constants;
-import in.pecfest.www.pecfest.Model.Common.Request;
-import in.pecfest.www.pecfest.Model.Common.Response;
 import in.pecfest.www.pecfest.Model.MegaShows.MegaResponse;
 import in.pecfest.www.pecfest.R;
+import in.pecfest.www.pecfest.Utilites.Results;
 import in.pecfest.www.pecfest.Utilites.Utility;
 
 /**
  * Created by Abhi on 06-09-2016.
  */
-public class ShowsFragment extends Fragment implements CommunicationInterface {
+public class ShowsFragment extends Fragment {
 
     ImageView iv1;
     TextView tx1, tx2;
@@ -43,43 +43,49 @@ public class ShowsFragment extends Fragment implements CommunicationInterface {
         tx1 = (TextView) rootView.findViewById(R.id.shows_title);
         tx2 = (TextView) rootView.findViewById(R.id.shows_desc);
 
-        Request r = new Request();
-        r.method = Constants.METHOD.GET_MEGA_SHOWS;
-        r.showPleaseWaitAtStart=true;
-        r.hidePleaseWaitAtEnd = false;
-        r.heading = "GETTING SHOWS";
-        Utility.SendRequestToServer(getContext(),r);
+        new getShow().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+
         return rootView;
     }
 
-    @Override
-    public void onRequestCompleted(String method, Response rr) {
-        if(rr.isSuccess==false)
-        {
-            Toast.makeText(getContext(), rr.errorMessage, Toast.LENGTH_LONG).show();
+
+    class getShow extends AsyncTask<Void, Void, Void>{
+
+        MegaResponse mr;
+        @Override
+        protected Void doInBackground(Void...v){
+
+            try {
+                HttpConnection hc = new HttpConnection(Utility.getBaseUrl(getContext()), 1);
+                hc.putBody("{\"method\": \"" + Constants.METHOD.GET_MEGA_SHOWS + "\"}");
+                Results rts = hc.getData();
+                mr = (MegaResponse)Utility.getObjectFromJson(rts.data,MegaResponse.class);
+            }
+            catch(Exception e){
+                Log.v("Error", e.getMessage());
+            }
+            return null;
         }
-        if(method.equals(Constants.METHOD.GET_MEGA_SHOWS))
-        {
-            MegaResponse mr = null;
+
+        protected void onPostExecute(Void v){
             try{
-                 mr = (MegaResponse)Utility.getObjectFromJson(rr.JsonResponse,MegaResponse.class);
                 if(showDay==1)
                 {
-                    ImageLoader i = new ImageLoader(mr.url1,iv1,1,false);
+                    ImageLoader i = new ImageLoader("http://"+mr.url1,iv1,1,false);
                     i.execute();
                     tx1.setText(mr.title1);
                     tx2.setText(mr.des1);
                 }
                 if(showDay==2)
                 {
-                    ImageLoader i = new ImageLoader(mr.url2,iv1,1,false);
+                    ImageLoader i = new ImageLoader("http://"+mr.url2,iv1,1,false);
                     i.execute();
                     tx1.setText(mr.title2);
                     tx2.setText(mr.des2);
                 }
                 if(showDay==3)
                 {
-                    ImageLoader i = new ImageLoader(mr.url3,iv1,1,false);
+                    ImageLoader i = new ImageLoader("http://"+mr.url3,iv1,1,false);
                     i.execute();
                     tx1.setText(mr.title3);
                     tx2.setText(mr.des3);
@@ -87,8 +93,8 @@ public class ShowsFragment extends Fragment implements CommunicationInterface {
             }
             catch (Exception e)
             {
+                Log.v("Error2", e.getMessage());
             }
-
         }
     }
 }
