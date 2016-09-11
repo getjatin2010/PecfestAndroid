c11d7d9c71d469cf1532e75c4d6c25ca9984ccf7
Abhinandan <abhics27@gmail.com>
2016-09-11 19:44:36 +0530

    NA-0: Working notifications

--- app/src/main/java/in/pecfest/www/pecfest/Activites/HomeScreen.java
+++ app/src/main/java/in/pecfest/www/pecfest/Activites/HomeScreen.java
@@ -29,7 +29,6 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -323,7 +322,7 @@ public class HomeScreen extends AppCompatActivity implements NavigationView.OnNa
                     case "Shows":
                         startActivity(new Intent(getApplicationContext(), Events.class).putExtra("title","Shows"));
                         break;
-                    case "Lecture":
+                    case "Lecture & Workshops":
                         startActivity(new Intent(getApplicationContext(), Events.class).putExtra("title","Lectures"));
                         break;
                     case "Register":
--- app/src/main/java/in/pecfest/www/pecfest/Notifications/MyFirebaseMessagingService.java
+++ app/src/main/java/in/pecfest/www/pecfest/Notifications/MyFirebaseMessagingService.java
@@ -4,10 +4,12 @@ import android.app.NotificationManager;
 import android.graphics.Bitmap;
 import android.support.v7.app.NotificationCompat;
 import android.util.Log;
-
+import android.app.PendingIntent;
+import android.content.Intent;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import in.pecfest.www.pecfest.Activites.Notification;
 import in.pecfest.www.pecfest.R;
 import in.pecfest.www.pecfest.Utilites.Utility;
 import in.pecfest.www.pecfest.Utilites.getBitmap;
@@ -72,10 +74,15 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         else{
             int c=0;
             if((c=Utility.getNewNotifs(this)-1)>1)
-                mBuilder.setContentText("  "+c+" new notifications");
+                mBuilder.setContentText("   +"+c+" new notifications");
             else
                 mBuilder.setContentText(body);
         }
+
+        Intent myIntent= new Intent(this, Notification.class);
+        PendingIntent intent2 = PendingIntent.getActivity(this, 1,
+                myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(intent2);
         // Gets an instance of the NotificationManager service
         NotificationManager mNotifyMgr =
                 (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
